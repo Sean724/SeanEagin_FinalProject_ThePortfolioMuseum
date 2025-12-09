9482d83b2e3e68a88fca56ae1022c5faf18c2c9b
seane <sean.eagin.animation@gmail.com>
2025-12-08 20:20:45 -0600

    Made all the input checks caps or lower case friendly

--- src/PortfolioMuseum.py
+++ src/PortfolioMuseum.py
@@ -5,17 +5,25 @@ def main():
     time.sleep(1)
     print ("Here we have various works of art on display created by yours truly.")
     time.sleep(1)
+    Wings()
+
+def Wings():
     print ("Where would you like to explore?")
     time.sleep(1)
-    Wing_Choice = input ("Personal, School, or Map? ")
-    if Wing_Choice == ("Personal"):
+    Wing_Choice = input ("Personal, School, or 2D? ")
+    Wing_Choice = Wing_Choice.lower()
+    if Wing_Choice == ("personal"):
         PersonalWing()
-    elif Wing_Choice == ("School"):
+    elif Wing_Choice == ("school"):
         SchoolWing()
+    elif Wing_Choice == ("2d"):
+        finalwing()
+    else:
+        Wings()
 
 def PersonalWing():
     time.sleep(1)
-    print ("Welcome to the North Wing of the museum!")
+    print ("Welcome to the East Wing of the museum!")
     time.sleep(1)
     print ("This is where all the 3D models done in my spare time reside.")
     time.sleep(1)
@@ -26,14 +34,15 @@ def PersonalWingQuestions():
     print ("Would you like to take a look at our pieces here?")
     time.sleep(1)
     PersonalWingChoice = input ("Y or N? ")
-    if PersonalWingChoice == ("Y"):
+    PersonalWingChoice = PersonalWingChoice.lower()
+    if PersonalWingChoice == ("y"):
         PersonalWingPieces()
-    elif PersonalWingChoice == ("N"):
+    elif PersonalWingChoice == ("n"):
         time.sleep(1)
         print ("Would you like to go to a different wing?")
         ChangeWingPersonal = input ("Y or N? ")
-        
-        if ChangeWingPersonal ==("Y"):
+        ChangeWingPersonal = ChangeWingPersonal.lower()
+        if ChangeWingPersonal ==("y"):
             main()
         else:
             PersonalWingQuestions()
@@ -44,25 +53,26 @@ def PersonalWingQuestions():
 def PersonalWingPieces():
     print ("What piece would you like to take a look at?")
     PersonalWingPieceChoice = input ("Rylee, Campbell, Andrea, Lidya, or Leave? ")
-    if PersonalWingPieceChoice == ("Campbell"):
+    PersonalWingPieceChoice = PersonalWingPieceChoice.lower()
+    if PersonalWingPieceChoice == ("campbell"):
         with Image.open ("Campbell Render.jpg") as img:
             img.show()
             img.close()
         LearnCampbell()
-    elif PersonalWingPieceChoice == ("Rylee"):
+    elif PersonalWingPieceChoice == ("rylee"):
         with Image.open ("Campbell Render.jpg") as img:
             img.show
-    elif PersonalWingPieceChoice == ("Lidya"):
+    elif PersonalWingPieceChoice == ("lidya"):
         with Image.open ("Lidya_Wideshot_Litup.jpg") as img:
             img.show()
             img.close()
             LearnLidya()
-    elif PersonalWingPieceChoice == ("Andrea"):
+    elif PersonalWingPieceChoice == ("andrea"):
         with Image.open ("AndreaGift_CloseUp.jpg") as img:
             img.show()
             img.close()
             LearnAndrea()
-    elif PersonalWingPieceChoice == ("Leave"):
+    elif PersonalWingPieceChoice == ("leave"):
         main()
     else:
         print ("Sorry, I didn't catch that, let me repeat myself.")
@@ -71,7 +81,8 @@ def PersonalWingPieces():
 def LearnCampbell():
     print ("Would you like to learn more?")
     LearnMoreCampbell = input ("Y or N? ")
-    if LearnMoreCampbell == ("Y"):
+    LearnMoreCampbell = LearnMoreCampbell.lower()
+    if LearnMoreCampbell == ("y"):
         print ("I made this piece for my friend Campbell's birthday back in 2024.")
         print ("I wanted to capture all her favorite interests in this model.")
         print ("I implemented her love of reading via the book model.")
@@ -79,7 +90,7 @@ def LearnCampbell():
         print ("Then I implemented her love for games with the meeple and the die.")
         print ("Finally I implemented her favorite color combo in the name model.")
         PersonalWingPieces()
-    elif LearnMoreCampbell == ("N"):
+    elif LearnMoreCampbell == ("n"):
         PersonalWingPieces()
     else:
         print ("Sorry, I didn't catch that, let me repeat myself.")
@@ -87,7 +98,8 @@ def LearnCampbell():
 def LearnLidya():
     print ("Would you like to learn more?")
     LearnMoreLidya = input ("Y or N? ")
-    if LearnMoreLidya == ("Y"):
+    LearnMoreLidya = LearnMoreLidya.lower()
+    if LearnMoreLidya == ("y"):
         print ("I made this piece for my friend Lidya's birthday back in 2024.")
         print ("I wanted to capture all her favorite interests in this model.")
         print ("I implemented her love of movies via the camera and projector screen.")
@@ -95,27 +107,49 @@ def LearnLidya():
         print ("Then I implemented her love for reptiles with the lizard")
         print ("Finally I implemented her favorite color combo in the name model.")
         PersonalWingPieces()
-    elif LearnMoreLidya == ("N"):
+    elif LearnMoreLidya == ("n"):
         PersonalWingPieces()
     else:
         print ("Sorry, I didn't catch that, let me repeat myself.")
         LearnLidya()
 
-
-def LearnAndrea():
+def LearnRylee():
     print ("Would you like to learn more?")
-    LearnMoreAndrea = input ("Y or N? ")
-    if LearnMoreAndrea == ("Y"):
+    LearnMoreRylee = input ("Y or N? ")
+    LearnMoreRylee = LearnMoreRylee.lower()
+    if LearnMoreRylee == ("y"):
         print ("I made this piece for my friend Andrea's birthday back in 2024.")
         print ("I wanted to capture all her favorite interests in this model.")
         print ("The main thing I implemented here was her love of cats as it was the main bit of info I had going into it.")
         print ("But I also implemented her favorite color combo in the name model.")
         PersonalWingPieces()
-    elif LearnMoreAndrea == ("N"):
+    elif LearnMoreRylee == ("n"):
         PersonalWingPieces()
     else:
         print ("Sorry, I didn't catch that, let me repeat myself.")
-        LearnLidya()
+        LearnRylee()
+
+def LearnAndrea():
+    print ("Would you like to learn more?")
+    LearnMoreAndrea = input ("Y or N? ")
+    LearnMoreAndrea = LearnMoreAndrea.lower()
+    if LearnMoreAndrea == ("y"):
+        print ("I made this piece for my friend Andrea's birthday back in 2024.")
+        print ("I wanted to capture all her favorite interests in this model.")
+        print ("The main thing I implemented here was her love of cats as it was the main bit of info I had going into it.")
+        print ("But I also implemented her favorite color combo in the name model.")
+        PersonalWingPieces()
+    elif LearnMoreAndrea == ("n"):
+        PersonalWingPieces()
+    else:
+        print ("Sorry, I didn't catch that, let me repeat myself.")
+        LearnAndrea()
+
+def finalwing():
+    print ("Welcome to the North Wing of the museum!")
+    print ("This is where all the non 3D Model artwork resides")
+    print ("They may not be my strength area, but I've put in a lot of effort all the same.")
+    SchoolWingQuestions()
 
 def SchoolWing():
     print ("Welcome to the West Wing of the museum!")
